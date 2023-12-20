f9764a9760a284914488734e5a8c87adc250a480
adityatadas24 <adityatadas2000@gmail.com>
2023-12-20 20:56:53 +0530

    submitting code-Wed Dec 20 2023 20:56:52 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,82 @@
-import React, {useState} from "react";
+import React, { useState} from "react";
 import '../styles/App.css';
 
-const arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
-
-const App = () => {
-  const [name1, setName1] = useState("");
-  const [name2, setName2] = useState("");
-  const [relationship, setRelationship] = useState("");
-  const [btnClicked, setBtnClicked] = useState(false);
-
-  console.log(name1, name2);
-
-  function calculateRelationship(e) {
-    e.preventDefault();
-
-    if (name1.trim() === "" || name2.trim() === "") {
-      setBtnClicked(false);
-      setRelationship("Please Enter valid input");
-      return
-    }
-
-    let str1 = name1;
-    let str2 = name2;
-    for (let t of str1) {
-      if (str2.includes(t)) {
-        str1 = str1.replace(t, "");
-        str2 = str2.replace(t, "");
+  const arr = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
+  const App = () => {
+    const [name1, setName1] = useState("");
+    const [name2, setName2] = useState("");
+    const [relationship, setRelationship] = useState("");
+    const [btnClicked, setBtnClicked] = useState(false);
+  
+    console.log(name1, name2);
+  
+    function calculateRelationship(e) {
+      e.preventDefault();
+  
+      if (name1.trim() === "" || name2.trim() === "") {
+        setBtnClicked(false);
+        setRelationship("Please Enter valid input");
+        return;
       }
+  
+      let str1 = name1;
+      let str2 = name2;
+      for (let t of str1) {
+        if (str2.includes(t)) {
+          str1 = str1.replace(t, "");
+          str2 = str2.replace(t, "");
+        }
+      }
+      setName1(str1);
+      setName2(str2);
+      setBtnClicked(true);
+      setRelationship(arr[(name1.length + name2.length) % 6]);
     }
-    setName1(str1);
-    setName2(str2);
-    setBtnClicked(true);
-    setRelationship(arr[(str1.length + str2.length)%6]);
-  }
-
-  return (
-    <div id="main">
-      <form>
-        <input
-          type="text"
-          name="name1"
-          data-testid="input1"
-          value={name1}
-          placeholder="Enter First Name"
-          onChange={(e) => setName1(e.target.value)}
-        />
-
-        <input type="text"
-          name="name2"
-          data-testid="input2"
-          value={name2}
-          placeholder="Enter Second Name"
-          onChange={(e)=>setName2(e.target.value)}
-        />
-
-        <button data-testid="calculate_relationship" type="submit"
-          onClick={calculateRelationship}
-        >Calculate Relationship Future</button>
-
-        <button data-testid="clear" type="reset"
-          onClick={()=>{
-            setName1("");
-            setName2("");
-            setBtnClicked(false);
-            setRelationship("");
-          }}
-        >Clear</button>
-      </form>
-
-      <h3 data-testid="answer">{relationship}</h3>
-    </div>
-  );
-};
-
+  
+    return (
+      <div id="main">
+        <form>
+          <input
+            type="text"
+            name="name1"
+            data-testid="input1"
+            value={name1}
+            onChange={(e) => setName1(e.target.value)}
+          />
+  
+          <input
+            type="text"
+            name="name2"
+            data-testid="input2"
+            value={name2}
+            onChange={(e) => setName2(e.target.value)}
+          />
+  
+          <button
+            data-testid="calculate_relationship"
+            type="submit"
+            onClick={calculateRelationship}
+          >
+            Calculate Relationship Future
+          </button>
+  
+          <button
+            data-testid="clear"
+            type="reset"
+            onClick={() => {
+              setName1("");
+              setName2("");
+              setBtnClicked(false);
+              setRelationship("");
+            }}
+          >
+            Clear
+          </button>
+        </form>
+  
+        <h3 data-testid="answer">{relationship}</h3>
+      </div>
+    );
+  };
 
 export default App;
